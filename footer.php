f8f04ac2d969afbb8d1a83a3467adb7a531fc94a
Robin Hamill <hamillrobin@gmail.com>
2015-08-18 15:14:56 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package RH_portfolio
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rh_portfolio' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'rh_portfolio' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'rh_portfolio' ), 'rh_portfolio', '<a href="http://underscores.me/" rel="designer">Robin Hamill</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
