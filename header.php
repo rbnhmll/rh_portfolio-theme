a55c099c79c4e7b50002b043133dd41bc32957fe
Robin Hamill <hamillrobin@gmail.com>
2015-08-20 10:13:08 -0400

    addition of dynamic blog section

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?>  id="home top">
 <div id="page" class="hfeed site">
 	<!-- <a class="skip-link screen-reader-text" href="#content"><?php //esc_html_e( 'Skip to content', 'rh_portfolio' ); ?></a> -->
 
--- page-home.php
+++ page-home.php
@@ -32,7 +32,7 @@ get_header(); ?>
 
 
 <!-- ABOUT SECTION -->
-	<section class="about-container">
+	<section class="about-container" id="about">
 		<div class="inner-wrapper">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<h3><?php the_field('about_section_title'); ?></h3>
@@ -47,7 +47,7 @@ get_header(); ?>
 
 
 <!-- PORTFOLIO SECTION -->
-	<section class="portfolio-container">
+	<section class="portfolio-container" id="portfolio">
 		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<div class="inner-wrapper">
 					<h3><?php the_field('portfolio_section_title'); ?></h3>
@@ -58,33 +58,66 @@ get_header(); ?>
 
 
 <!-- BLOG SECTION -->
-	<section class="blog-container">
+	<section class="blog-container" id="blog">
 		<div class="inner-wrapper">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 				<h3><?php the_field('blog_section_title'); ?></h3>
 				<p><?php the_field('blog_description'); ?></p>
+				<div class="blog-posts">
+					<?php $args = array(
+						'sort_order' => 'asc',
+						'sort_column' => 'menu_order',
+						'post_type' => 'post',
+						'post_status' => 'publish',
+						'posts_per_page' => 3
+					); 
+					?>
+
+					<?php $blogQuery = new WP_Query( $args ); ?>
+
+					<?php if ( $blogQuery->have_posts() ) : ?>
+
+					<?php while ( $blogQuery->have_posts() ) : $blogQuery->the_post(); ?>
+							<div class="blog-post">
+								<?php the_title(); ?>
+								<?php the_content(); ?>
+							</div>
+					<?php endwhile; ?>
+
+					<?php wp_reset_postdata(); ?>
+
+					<?php else:  ?>
+						
+					<?php endif; ?>
+				</div> <!-- /.blog-posts -->
 			<?php endwhile; ?>
 		</div> <!-- /.inner-wrapper -->
 	</section> <!-- /.blog-container -->
 
 
 <!-- CONTACT SECTION -->
-	<section class="contact-container">
+	<section class="contact-container" id="contact">
 		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 			<div class="inner-wrapper">
 				<h3><?php the_field('contact_section_title'); ?></h3>
 				<p><?php the_field('contact_description'); ?></p>
 			</div> <!-- /.inner-wrapper -->
 			<div class="outer-wrapper">
-				<div class="information"></div>
+				<div class="information">
+					<h4>Information</h4>
+					<p><i class="fa fa-mobile"></i><?php the_field('phone'); ?></p>
+					<p><i class="fa fa-envelope-o"></i><?php the_field('email'); ?></p>
+				</div> <!-- /.information -->
+				
 				<form action="" class="contact-form">
-					<input type="text" class="name-input">
-					<input type="email" class="email-input">
-					<textarea name="" id="" cols="30" rows="10"></textarea>
+					<input type="text" class="name-input" placeholder="Name">
+					<input type="email" class="email-input" placeholder="E-Mail">
+					<textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
 					<input type="submit" value="Send Message" class="submit-button">
 				</form>
 			</div> <!-- /.outer-wrapper -->
 			<?php endwhile; ?>
+		<a href="#top" class="back-to-top"><i class="fa fa-angle-up"></i></a>
 	</section> <!-- /.contact-container -->
 
 <?php //get_sidebar(); ?>
